9141c9df0281badc782db20c1cdc9233704e9a57
Vera <catharsis1607@gmail.com>
2021-12-21 00:11:40 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Написать функцию нахождения минимального числа
+# без использования условного оператора, циклов и функции min.
+
+def minimum(x, y):
+  z = (x + y - abs(x - y)) / 2
+  return z
+
+number_first = int(input('Введите первое число: '))
+number_twice = int(input('Введите второе число: '))
+
+minimum(number_first, number_twice)
+
+print('Наибольшее число:', z)
